859b4684fc9194116e6b7b319c9bedda68a03bbe
tshemsedinov <timur.shemsedinov@gmail.com>
2017-11-07 19:27:56 +0000

    Add quick functor implementation

---
+++ JavaScript/f-functor.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const util = require('util');
+
+function Functor() {
+}
+
+Functor.prototype.method = function() {
+  console.log('Functor.prototype.method');
+  console.dir({
+    property: this.property,
+    exec: this.method
+  });
+  this(); // you can call this as a function
+};
+
+util.inherits(Functor, Function);
+
+const factory = () => {
+
+  const functor = function() {
+    console.log('functor');
+    functor.property = 'value';
+    console.dir({
+      property: functor.property,
+      method: functor.method
+    });
+  };
+
+  Object.setPrototypeOf(functor, Functor.prototype);
+  return functor;
+};
+
+// Usage
+
+const fn = factory();
+fn();
+fn.method();
