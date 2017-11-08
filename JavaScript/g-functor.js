8d2f375e15e80d0bc7d035f415eb0fec54ca267c
tshemsedinov <timur.shemsedinov@gmail.com>
2017-11-08 05:08:58 +0200

    Quick functor with class

---
+++ JavaScript/g-functor.js
@@ -0,0 +1,33 @@
+'use strict';
+
+class Functor extends Function {
+  method() {
+    console.log('Functor.prototype.method');
+    console.dir({
+      property: this.property,
+      method: this.method
+    });
+    this(); // you can call this as a function
+  }
+}
+
+const factory = () => {
+
+  const functor = function() {
+    console.log('functor');
+    functor.property = 'value';
+    console.dir({
+      property: functor.property,
+      method: functor.method
+    });
+  };
+
+  Object.setPrototypeOf(functor, Functor.prototype);
+  return functor;
+};
+
+// Usage
+
+const fn = factory();
+fn();
+fn.method();
