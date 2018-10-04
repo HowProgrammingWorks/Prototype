1c1850e4ad875feb5ba4c356f6cdc352081b39a6
tshemsedinov <timur.shemsedinov@gmail.com>
2018-10-04 21:50:37 +0300

    Use setPrototypeOf instead of node.js utils

--- JavaScript/9-inherits.js
+++ JavaScript/9-inherits.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const util = require('util');
-
 function Rect(x, y, width, height) {
   this.x = x;
   this.y = y;
@@ -17,15 +15,16 @@ function Square(x, y, side) {
   Rect.call(this, x, y, side, side);
 }
 
-util.inherits(Square, Rect);
+Object.setPrototypeOf(Square.prototype, Rect.prototype);
+
+// const util = require('util');
+// util.inherits(Square, Rect);
 
 // Square.prototype = Object.create(Rect.prototype);
 // Square.prototype.constructor = Square;
 
 // Square.prototype = new Rect();
 // Square.prototype.constructor = Square;
-
-// Object.setPrototypeOf(Square.prototype, Rect.prototype);
 
 const p1 = new Square(10, 20, 50);
 console.log(p1.toString());
